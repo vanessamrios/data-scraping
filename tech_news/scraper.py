527405695fb92bf448759ca3df1e8af8d6c404d2
Vanessa <vanessamrios@gmail.com>
2022-08-25 21:55:31 +0200

    feat: implementa a função scrape_noticia que recupera dados de uma notícia e amazena em um dicionário

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -6,7 +6,9 @@ from parsel import Selector
 # Requisito 1
 def fetch(url: str, wait: int = 3) -> str:
     try:
-        response = requests.get(url, timeout=wait)
+        response = requests.get(
+            url, timeout=wait, headers={"user-agent": "Fake user-agent"}
+        )
         time.sleep(1)
         if response.status_code != 200:
             response.raise_for_status()
@@ -34,8 +36,43 @@ def scrape_next_page_link(html_content: str) -> str or None:
 
 
 # Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+
+
+def scrape_noticia(html_content: str) -> dict:
+    selector = Selector(html_content)
+    noticia = {
+        "url": "",
+        "title": "",
+        "timestamp": "",
+        "writer": "",
+        "comments_count": 0,
+        "summary": "",
+        "tags": [],
+        "category": ""
+    }
+    url = selector.xpath("//link[@rel='canonical']/@href").get()
+    title = selector.css('h1.entry-title::text').get()
+    timestamp = selector.css('li.meta-date::text').get()
+    writer = selector.css('a.url.fn.n::text').get()
+    coments_count = len(
+        selector.xpath('//li[starts-with(@id, "comment")]').getall()
+    )
+    sumary = "".join(
+        selector.css('div.entry-content').css('p')[0].css('::text').getall()
+    )
+    tags = selector.xpath('//a[contains(@rel, "tag")]').css('::text').getall()
+    category = selector.css('span.label::text').get()
+    noticia.update({
+        "url": url,
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": coments_count,
+        "summary": sumary.strip(),
+        "tags": tags,
+        "category": category
+    })
+    return noticia
 
 
 # Requisito 5
