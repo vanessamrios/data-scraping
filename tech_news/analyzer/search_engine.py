0ca7ac12437e52b5c953505864ceb0ff3c7bed58
Vanessa <vanessamrios@gmail.com>
2022-08-29 11:38:45 +0200

    feat: implementa a função search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -42,4 +42,11 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    tuple_list = []
+    noticias = search_news(
+        {"category": {'$regex': category, '$options': 'i'}}
+    )
+    for noticia in noticias:
+        if (noticia not in tuple_list):
+            tuple_list.append((noticia["title"], noticia["url"]))
+    return tuple_list
